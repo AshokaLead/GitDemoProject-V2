17cea3d5f4c57d0668658d5743d56cc13508f99c
ashoka <ashoka.devara89@gmail.com>
2020-03-29 20:10:53 +0530

    adding GitDemoProject-V2 First commit....

---
+++ src/main/java/com/ashoka/GitDemoProjectV2/GitDemoProjectV2Application.java
@@ -0,0 +1,13 @@
+package com.ashoka.GitDemoProjectV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitDemoProjectV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitDemoProjectV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashoka/GitDemoProjectV2/GitDemoProjectV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashoka.GitDemoProjectV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoProjectV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
