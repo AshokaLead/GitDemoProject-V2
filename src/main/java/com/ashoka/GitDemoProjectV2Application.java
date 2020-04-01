af29ca2a2de78e1879856a0eef139a3823b2ef99
ashoka <ashoka.devara89@gmail.com>
2020-04-01 13:42:22 +0530

    adding Employee, TestController classes

--- src/main/java/com/ashoka/GitDemoProjectV2Application.java
+++ src/main/java/com/ashoka/GitDemoProjectV2Application.java
@@ -1,4 +1,4 @@
-package com.ashoka.GitDemoProjectV2;
+package com.ashoka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,5 @@ public class GitDemoProjectV2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(GitDemoProjectV2Application.class, args);
 	}
-
+	
 }
